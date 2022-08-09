dba7a7c82f028987ac6c3ba68056e3fc86992b34
Villanueva Brandan Zomeli <zomeli.villanueva@gmail.com>
2022-08-09 04:26:40 -0500

    [FEAT] Update to proyect.

--- src/test/java/com/zomeli/villanueva/apirestpokeapi/retrofit/ApiRestPokeApiApplicationTests.java
+++ src/test/java/com/zomeli/villanueva/apirestpokeapi/retrofit/ApiRestPokeApiApplicationTests.java
@@ -1,13 +1,19 @@
-package com.zomeli.villanueva.apirestpokeapi;
+package com.zomeli.villanueva.apirestpokeapi.retrofit;
 
 import com.zomeli.villanueva.apirestpokeapi.model.pokeapi.PokeApiEvolutionChain;
+import com.zomeli.villanueva.apirestpokeapi.model.pokeapi.PokeApiNamedResourceList;
+import com.zomeli.villanueva.apirestpokeapi.model.pokeapi.PokeApiPokemon;
 import com.zomeli.villanueva.apirestpokeapi.proxy.contract.ServiceHttpClientPokeApi;
 import io.reactivex.Single;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import reactor.test.StepVerifier;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -29,18 +35,15 @@ public class ApiRestPokeApiApplicationTests {
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
-
         service = retrofit.create(ServiceHttpClientPokeApi.class);
 
-//        Single<Response<PokeApiNamedResourceList>> retrofitCall = service.list(5,0);
+        Single<Response<PokeApiNamedResourceList>> retrofitCallListPokemon = service.list(5,0);
+        Single<PokeApiPokemon> retrofitCallPokemon = service.getPokemon("8");
+        Single<Response<PokeApiEvolutionChain>> retrofitCallEvolution = service.getEvolution("10");
 
-//        Single<PokeApiPokemon> retrofitCall = service.getPokemon("8");
-
-        Single<Response<PokeApiEvolutionChain>> retrofitCall = service.getEvolution("10");
-
-        retrofitCall.subscribe(m -> System.out.println(m.body()));
-        System.out.println(retrofitCall);
-
+        retrofitCallListPokemon.subscribe(m -> System.out.println(m.body()));
+        retrofitCallPokemon.subscribe(m -> System.out.println(m));
+        retrofitCallEvolution.subscribe(m -> System.out.println(m.body()));
     }
 
 }
